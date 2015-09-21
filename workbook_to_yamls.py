54269294275dcf44fb56d20c62ec95ea7b311e40
ramirezg <gabriel.ramirez@gsa.gov>
2015-09-21 13:07:41 -0400

    making the script write data to yaml files

--- workbook_to_yamls.py
+++ workbook_to_yamls.py
@@ -1,7 +1,10 @@
 """ This script coverts the Control-masonry excel file to
 a series of yamls """
 
+import os
+
 from openpyxl import load_workbook
+from yaml import dump
 
 
 class ComponentMissingError(Exception):
@@ -26,7 +29,7 @@ def open_workbook(filename='Control-masonry.xlsx'):
 def extract_components(workbook):
     """ Get the individual components from the xlsx workbook """
     components = dict()
-    for row in workbook['Components'].rows:
+    for row in workbook['Components'].rows[1:]:
         component_id = row[1].value
         component_name = row[2].value
         if not components.get(component_id):
@@ -38,7 +41,7 @@ def extract_components(workbook):
 def layer_with_references(components, workbook):
     """ Extract the components worksheet information from the `References`
     worksheet and place the data into a dict """
-    for row in workbook['References'].rows:
+    for row in workbook['References'].rows[1:]:
         component_id = row[0].value
         reference_name = row[1].value
         reference_url = row[2].value
@@ -58,7 +61,7 @@ def layer_with_references(components, workbook):
 
 def layer_with_governors(components, workbook):
     """ Layer the components data with data from the governors spreadsheet """
-    for row in workbook['Governors'].rows:
+    for row in workbook['Governors'].rows[1:]:
         component_id = row[0].value
         governor_name = row[1].value
         governor_url = row[2].value
@@ -79,7 +82,7 @@ def layer_with_governors(components, workbook):
 def layer_with_justifications(components, workbook):
     """ Layer the components data with data from the `Justifications`
     spreadsheet to show which controls each componenet satisfies """
-    for row in workbook['Justifications'].rows:
+    for row in workbook['Justifications'].rows[1:]:
         control_id = row[0].value
         component_id = row[2].value
         narrative = row[3].value
@@ -97,7 +100,7 @@ def layer_with_justifications(components, workbook):
 def split_into_systems(components, workbook):
     """ Splits the individual components into systems """
     systems = dict()
-    for row in workbook['Components'].rows:
+    for row in workbook['Components'].rows[1:]:
         system_id = row[0].value
         component_id = row[1].value
         if not systems.get(system_id):
@@ -106,8 +109,9 @@ def split_into_systems(components, workbook):
     return systems
 
 
-def create_yamls():
-    """ Create yaml files containing data from the control masonry workbook """
+def process_data():
+    """ Collect data from the xlsx workbook and structure data
+    in dict by system and then component """
     workbook = open_workbook()
     components = extract_components(workbook=workbook)
     components = layer_with_references(
@@ -121,5 +125,35 @@ def create_yamls():
     return systems
 
 
+def create_folder(directory):
+    """ Create a folder if it doesn't exist """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def write_yaml_data(component_data, filename):
+    """ Write component data to a yaml file """
+    with open(filename, 'w') as yaml_file:
+        yaml_file.write(dump(component_data, default_flow_style=False))
+
+
+def export_yamls(data, base_dir='data'):
+    """ Create a series of yaml files for each component organized
+    by system """
+    create_folder(base_dir)
+    systems = data.keys()
+    for system in systems:
+        directory = os.path.join(base_dir, system.replace(' ', ''))
+        create_folder(directory)
+        components = data[system].keys()
+        for component in components:
+            filename = os.path.join(
+                directory, component.replace(' ', '') + '.yaml')
+            write_yaml_data(
+                component_data=data[system][component],
+                filename=filename
+            )
+
+
 if __name__ == "__main__":
-    create_yamls()
+    export_yamls(data=process_data())
