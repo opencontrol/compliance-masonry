b022c2d12ed1b745de2c455e0cb71f6e5b9cdbe7
ramirezg <gabriel.ramirez@gsa.gov>
2015-10-29 21:29:13 -0400

    adding template files inside module

--- setup.py
+++ setup.py
@@ -24,24 +24,31 @@ setup(
             'masonry = src.cli:main',
         ],
     },
+    package_data={
+        'src': [
+            'templates/certifications/*',
+            'templates/components/*/*',
+            'templates/standards/*',
+        ]
+    },
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
-        # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX',
+        # 'Operating System :: POSIX',
         'Operating System :: MacOS',
         'Operating System :: Unix',
-        'Operating System :: Windows',
+        # 'Operating System :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
