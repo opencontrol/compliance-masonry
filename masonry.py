56e13f058f9fe9741bd51a08836c702dcf45cfd1
ramirezg <gabriel.ramirez@gsa.gov>
2015-11-05 16:49:59 -0500

    Inventory Builde: adding script, cli, and import masonry

--- masonry.py
+++ masonry.py
@@ -1,7 +1,7 @@
 """ An interface for the renderers to be used when importing """
 
 from src.renderers import (
-    yamls_to_certification, certifications_to_gitbook
+    yamls_to_certification, certifications_to_gitbook, inventory_builder
 )
 
 
@@ -21,3 +21,9 @@ def build_gitbook(certification, certification_dir, output_dir):
         certification_dir=certification_dir,
         output_path=output_dir
     )
+
+
+def build_inventory(certification_path):
+    """ Interface for inventory_builder renderer, which creates an inventory
+    of components for a specific certification """
+    return inventory_builder.build_inventory(certification_path)
---
+++ src/renderers/inventory_builder.py
@@ -0,0 +1,87 @@
+import os
+import yaml
+
+
+def yaml_loader(filename):
+    """ Load a yaml file """
+    with open(filename) as yaml_file:
+        return yaml.load(yaml_file)
+
+
+def yaml_writer(data, filename):
+    """ Write yaml file"""
+    with open(filename, 'w') as yaml_file:
+        yaml_file.write(yaml.dump(data, default_flow_style=False))
+
+
+def prepare_cert_path(certification, certification_dir):
+    """ Prepare the path for a specific certification """
+    if not certification_dir:
+        certification_dir = 'exports/certifications/'
+    return os.path.join(certification_dir, '{0}.yaml'.format(certification))
+
+
+def prepare_output_path(output_path):
+    """ Set output_path and create a content dir if needed """
+    if not output_path:
+        output_path = 'exports/inventory'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    return output_path
+
+
+def analyze_attribute(attribute):
+    """ Check how many elements an attribute has otherwise return "Missing """
+    if attribute:
+        return len(attribute)
+    return "Missing"
+
+
+def analyze_component(component):
+    """ Analyze a component to find gaps in governors and references """
+    return {
+        'references': analyze_attribute(component.get('references')),
+        'governors': analyze_attribute(component.get('governors')),
+    }
+
+
+def catalog_control(inventory, control, standard_key, control_key):
+    """ Adds all the components in the control into the inventory
+    while determing the gaps """
+    if 'justifications' in control:
+        for component in control['justifications']:
+            system = component.get('system', 'No System')
+            name = component.get('name', 'No Name')
+            if system not in inventory[standard_key][control_key]:
+                inventory[standard_key][control_key][system] = {}
+            analysis = analyze_component(component)
+            inventory[standard_key][control_key][system][name] = analysis
+    else:
+        inventory[standard_key][control_key] = "Missing Justifications"
+
+
+def build_inventory(certification_path):
+    """ Create an inventory of components for a specific certification """
+    certification = yaml_loader(certification_path)
+    inventory = {'certification': certification.get('name')}
+    for standard_key in certification['standards']:
+        inventory[standard_key] = {}
+        for control_key in certification['standards'][standard_key]:
+            inventory[standard_key][control_key] = {}
+            control = certification['standards'][standard_key][control_key]
+            catalog_control(inventory, control, standard_key, control_key)
+    return inventory
+
+
+def create_inventory(certification, certification_dir, output_path):
+    certification_path = prepare_cert_path(certification, certification_dir)
+    if not os.path.exists(certification_path):
+        return None, "{} certification not found".format(certification)
+    output_path = prepare_output_path(output_path)
+    inventory = build_inventory(certification_path)
+    inventory_path = os.path.join(
+        output_path,
+        inventory.get('certification') + '.yaml'
+    )
+    yaml_writer(inventory, inventory_path)
+    return inventory_path, None
