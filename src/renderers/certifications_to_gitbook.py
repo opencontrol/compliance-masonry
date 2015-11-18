184f2665502f3efd6abb4a9a9aa575c830770268
ramirezg <gabriel.ramirez@gsa.gov>
2015-11-18 18:30:00 -0500

    updating certifications to gitbook to work with updated schema

--- src/renderers/certifications_to_gitbook.py
+++ src/renderers/certifications_to_gitbook.py
@@ -12,13 +12,9 @@ def write_markdown(output_path, filename, text):
         md_file.write(text)
 
 
-def convert_name_url(references):
-    """ Converts references data to markdown url bullet point. """
-    text = ''
-    for reference in references:
-        text += '\n* [{0}]({1})\n'.format(
-            reference['name'], reference['url'])
-    return text
+def convert_element(element):
+    """ Converts a dict with a name url and type to markdown """
+    return '\n[{0}]({1})\n'.format(element['name'], element['url'])
 
 
 def generate_text_narative(narative):
@@ -30,16 +26,16 @@ def generate_text_narative(narative):
         for key in sorted(narative):
             text += '{0}. {1} \n '.format(key, narative[key])
     else:
-        text = narative
+        text = narative + '  \n'
     return text
 
 
-def build_summary(summary, output_path):
+def build_summary(summaries, output_path):
     """ Construct a gitbook summary for the controls """
     main_summary = "# Summary\n\n"
     last_family = ''
     section_summary = ''
-    for control in summary:
+    for control in summaries['standards']:
         new_family = control['family']
         if last_family != new_family:
             control_family_name = control['control_name']
@@ -68,11 +64,28 @@ def build_summary(summary, output_path):
         last_family = new_family
     # Export the last family
     write_markdown(output_path, 'content/' + last_family + '.md', section_summary)
+    for system_key in sorted(summaries['components']):
+        main_summary += '* [{0}](content/{1}.md)\n'.format(system_key, system_key)
+        section_summary = '# {0}  \n###Components  \n'.format(system_key)
+        for component_key in sorted(summaries['components'][system_key]):
+            component = summaries['components'][system_key][component_key]
+            # Add the components url to main summary
+            main_summary += '\t* [{0}](content/{1}.md)\n'.format(
+                component['component_name'],
+                component['slug']
+            )
+            # Add the components url to section summary
+            section_summary += '* [{0}]({1}.md)\n'.format(
+                component['component_name'],
+                component['slug']
+            )
+        write_markdown(output_path, 'content/' + system_key + '.md', section_summary)
+
     write_markdown(output_path, 'SUMMARY.md', main_summary)
     write_markdown(output_path, 'README.md', main_summary)
 
 
-def document_page(summary, certification, standard_key, control_key):
+def document_cert_page(certification, standard_key, control_key):
     """ Create a new page dict. This item is a dictionary that
     contains the standard and control keys, a slug of the combined key, and the
     name of the control"""
@@ -87,29 +100,91 @@ def document_page(summary, certification, standard_key, control_key):
     }
 
 
-def create_content(control):
+def document_component_page(certification, system_key, component_key):
+    """ Create a new page dict. This item is a dictionary that
+    contains the standard and control keys, a slug of the combined key, and the
+    name of the control"""
+    component = certification['components'][system_key][component_key]
+    slug = slugify('{0}-{1}'.format(system_key, component_key))
+    return {
+        'system_key': system_key,
+        'component_key': component_key,
+        'component_name': component['name'],
+        'slug': slug
+    }
+
+
+def fetch_component(reference, certification):
+    """ Fetches a specific component from the certification dict,
+    this component will be used to extract the component name and it's verifications
+    when they are referenced """
+    return certification['components'][reference['system']][reference['component']]
+
+
+def fetch_verification(verification_ref, certification):
+    component = fetch_component(verification_ref, certification)['verifications']
+    return component[verification_ref['verification']]
+
+
+def org_by_system_component(justifications):
+    """ Organizes list of justifications in a dictionary of systems and
+    components """
+    justifications_dict = {}
+    for justification in justifications:
+        system_key = justification['system']
+        component_key = justification['component']
+        if system_key not in justifications_dict:
+            justifications_dict[system_key] = {}
+        justifications_dict[system_key][component_key] = justification
+    return justifications_dict
+
+
+def build_control_text(control, certification):
     """ Generate the markdown text from each `justification` """
     text = ''
-    for justification in control.get('justifications', []):
-        text += '\n## {0}\n'.format(justification.get('name'))
-        text += generate_text_narative(justification.get('narative'))
-        references = justification.get('references')
-        if references:
-            text += '\n### References\n'
-            text += convert_name_url(references)
-        governors = justification.get('governors')
-        if governors:
-            text += '\n### Governors\n'
-            text += convert_name_url(governors)
-        text += "\n--------\n"
+    # Order the justifications by system and then component
+    justifications_dict = org_by_system_component(control.get('justifications', []))
+    for system_key in justifications_dict:
+        text += '\n## {0}\n'.format(system_key)
+        for component_key in justifications_dict[system_key]:
+            justification = justifications_dict[system_key][component_key]
+            component = fetch_component(justification, certification)
+            text += '\n## {0}\n'.format(component.get('name'))
+            text += generate_text_narative(justification.get('narrative'))
+            verifications = justification.get('references')
+            if verifications:
+                for verification_ref in verifications:
+                    text += convert_element(
+                        fetch_verification(verification_ref, certification)
+                    )
     return text
 
 
-def build_page(page_dict, certification, output_path):
+def build_component_text(component):
+    """ Create markdown output for component text """
+    text = '\n### References  \n'
+    for reference in component.get('references', []):
+        text += convert_element(reference)
+    text += '\n### Verifications  \n'
+    for verification_key in component.get('verifications', []):
+            text += convert_element(component['verifications'][verification_key])
+    return text
+
+
+def build_cert_page(page_dict, certification, output_path):
     """ Write a page for the gitbook """
     text = '# {0}'.format(page_dict['control_name'])
     control = certification['standards'][page_dict['standard']][page_dict['control']]
-    text += create_content(control)
+    text += build_control_text(control, certification)
+    file_name = 'content/' + page_dict['slug'] + '.md'
+    write_markdown(output_path, file_name, text)
+
+
+def build_component_page(page_dict, certification, output_path):
+    """ Write a page for the gitbook """
+    text = '# {0}'.format(page_dict['component_name'])
+    component = certification['components'][page_dict['system_key']][page_dict['component_key']]
+    text += build_component_text(component)
     file_name = 'content/' + page_dict['slug'] + '.md'
     write_markdown(output_path, file_name, text)
 
@@ -123,15 +198,35 @@ def natural_sort(elements):
     return sorted(elements, key=alphanum_key)
 
 
-def create_gitbook_documentation(certification_path, output_path):
-    """ Convert certification to pages format """
+def build_standards_documentation(certification, output_path):
+    """ Create the documentation for standards """
     summary = []
-    certification = utils.yaml_loader(certification_path)
     for standard_key in natural_sort(certification['standards']):
         for control_key in natural_sort(certification['standards'][standard_key]):
             if 'justifications' in certification['standards'][standard_key][control_key]:
-                page_dict = document_page(summary, certification, standard_key, control_key)
+                page_dict = document_cert_page(certification, standard_key, control_key)
+                build_cert_page(page_dict, certification, output_path)
                 summary.append(page_dict)
-                build_page(page_dict, certification, output_path)
-    build_summary(summary, output_path)
+    return summary
+
+
+def build_components_documentation(certification, output_path):
+    """ Create the documentation for the components """
+    summary = {}
+    for system_key in certification['components']:
+        summary[system_key] = {}
+        for component_key in certification['components'][system_key]:
+            page_dict = document_component_page(certification, system_key, component_key)
+            build_component_page(page_dict, certification, output_path)
+            summary[system_key][component_key] = page_dict
+    return summary
+
+
+def create_gitbook_documentation(certification_path, output_path):
+    """ Convert certification to pages format """
+    summaries = {}
+    certification = utils.yaml_loader(certification_path)
+    summaries['standards'] = build_standards_documentation(certification, output_path)
+    summaries['components'] = build_components_documentation(certification, output_path)
+    build_summary(summaries, output_path)
     return output_path
