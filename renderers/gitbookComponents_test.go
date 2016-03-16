adf46d7d20734e7823aa3a7e4e4858832fa0eb6e
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-16 09:26:01 -0400

    Adding gitbook component constructor and associated tests

---
+++ renderers/gitbookComponents_test.go
@@ -0,0 +1,41 @@
+package renderers
+
+import (
+	"testing"
+
+	"github.com/opencontrol/compliance-masonry-go/models"
+)
+
+type componentExportTest struct {
+	component    models.Component
+	expectedPath string
+	expectedText string
+}
+
+var componentExportTests = []componentExportTest{
+	{
+		models.Component{
+			Name:          "Amazon Elastic Compute Cloud",
+			Key:           "EC2",
+			References:    []models.GeneralReference{{}},
+			Verifications: []models.VerificationReference{{}},
+			Satisfies:     []models.Satisfies{{}},
+			SchemaVersion: 2.0,
+		},
+		"EC2.md",
+		"# Amazon Elastic Compute Cloud  \n## References  \n* []()  \n## Verifications  \n* []()  \n",
+	},
+}
+
+func TestExportComponent(t *testing.T) {
+	for _, example := range componentExportTests {
+		gitbookComponent := ComponentGitbook{&example.component, ""}
+		actualPath, actualText := gitbookComponent.exportComponent()
+		if example.expectedPath != actualPath {
+			t.Errorf("Expected %s, Actual: %s", example.expectedPath, actualPath)
+		}
+		if example.expectedText != actualText {
+			t.Errorf("Expected %s, Actual: %s", example.expectedText, actualText)
+		}
+	}
+}
