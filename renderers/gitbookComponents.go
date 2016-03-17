db8de757b8fc4b704a523c0123ff2d85264dab9f
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-17 16:33:04 -0400

    Protecting against References and Verifications that are nil

--- renderers/gitbookComponents.go
+++ renderers/gitbookComponents.go
@@ -12,18 +12,22 @@ func (component *ComponentGitbook) exportComponent() (string, string) {
 	componentPath := component.Key + ".md"
 	text = fmt.Sprintf("# %s  \n", component.Name)
 	// Sort Components and Verifications
-	if component.References.Len() > 0 {
-		sort.Sort(component.References)
-		text += "## References  \n"
-		for _, reference := range *(component.References) {
-			text += exportLink(reference.Name, reference.Path)
+	if component.References != nil {
+		if component.References.Len() > 0 {
+			sort.Sort(component.References)
+			text += "## References  \n"
+			for _, reference := range *(component.References) {
+				text += exportLink(reference.Name, reference.Path)
+			}
 		}
 	}
-	if component.Verifications.Len() > 0 {
-		sort.Sort(component.Verifications)
-		text += "## Verifications  \n"
-		for _, reference := range *(component.Verifications) {
-			text += exportLink(reference.Name, reference.Path)
+	if component.Verifications != nil {
+		if component.Verifications.Len() > 0 {
+			sort.Sort(component.Verifications)
+			text += "## Verifications  \n"
+			for _, reference := range *(component.Verifications) {
+				text += exportLink(reference.Name, reference.Path)
+			}
 		}
 	}
 	return filepath.Join(component.exportPath, componentPath), text
