fddc5a987cd9d071f6037a8f8df5a8faa1f27df0
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-21 14:04:57 -0400

    Adding narratives to gitbook

--- gitbook/gitbookCertification.go
+++ gitbook/gitbookCertification.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"sort"
 
 	"github.com/opencontrol/compliance-masonry-go/helpers"
 	"github.com/opencontrol/compliance-masonry-go/models"
@@ -12,20 +13,30 @@ import (
 func (openControl *OpenControlGitBook) exportControl(control *ControlGitbook) (string, string) {
 	key := replaceParentheses(fmt.Sprintf("%s-%s", control.standardKey, control.controlKey))
 	text := fmt.Sprintf("#%s  \n##%s  \n", key, control.Name)
-
-	openControl.Justifications.GetAndApply(control.standardKey, control.controlKey, func(justifications models.Verifications) {
-		for _, justification := range justifications {
-			openControl.Components.GetAndApply(justification.Component, func(component *models.Component) {
-				if component != nil {
-					verification := component.Verifications.Get(justification.Verification)
-					if verification.Name != "" {
+	openControl.Justifications.GetAndApply(control.standardKey, control.controlKey, func(selectJustifications models.Verifications) {
+		sort.Sort(selectJustifications)
+		for _, justification := range selectJustifications {
+			openControl.Components.GetAndApply(justification.ComponentKey, func(component *models.Component) {
+				text += fmt.Sprintf("  \n#### %s  \n", component.Name)
+				text += fmt.Sprintf("%s  \n", justification.SatisfiesData.Narrative)
+			})
+			if len(justification.SatisfiesData.CoveredBy) > 0 {
+				text += "Covered By:  \n"
+			}
+			for _, coveredBy := range justification.SatisfiesData.CoveredBy {
+				componentKey := coveredBy.ComponentKey
+				if componentKey == "" {
+					componentKey = justification.ComponentKey
+				}
+				openControl.Components.GetAndApply(componentKey, func(component *models.Component) {
+					if component != nil {
 						text += exportLink(
-							fmt.Sprintf("%s - %s", component.Name, verification.Name),
+							fmt.Sprintf("%s - %s", component.Name, coveredBy.VerificationKey),
 							filepath.Join("..", "components", component.Key+".md"),
 						)
 					}
-				}
-			})
+				})
+			}
 		}
 	})
 	return filepath.Join(control.exportPath, key+".md"), text
--- models/justifications.go
+++ models/justifications.go
@@ -4,8 +4,8 @@ import "sync"
 
 // Verification struct holds data for a specific component and verification
 type Verification struct {
-	Component    string
-	Verification string
+	ComponentKey  string
+	SatisfiesData Satisfies
 }
 
 // Verifications is a slice of type Verifications
@@ -24,7 +24,7 @@ func (slice Verifications) Len() int {
 
 // Less returns true if a GeneralReference is less than another reference
 func (slice Verifications) Less(i, j int) bool {
-	return slice[i].Component+slice[i].Verification < slice[j].Component+slice[j].Verification
+	return slice[i].ComponentKey < slice[j].ComponentKey
 }
 
 // Swap swaps the two GeneralReferences
@@ -38,34 +38,27 @@ func NewJustifications() *Justifications {
 }
 
 // Add methods adds a new mapping to the justification while locking
-func (justifications *Justifications) Add(standardKey string, controlKey string, componentKey string, verificationKey string) {
+func (justifications *Justifications) Add(standardKey string, controlKey string, componentKey string, satisfies Satisfies) {
 	justifications.Lock()
+	newVerification := Verification{componentKey, satisfies}
 	_, standardKeyExists := justifications.mapping[standardKey]
 	if !standardKeyExists {
 		justifications.mapping[standardKey] = make(map[string]Verifications)
 	}
-
 	_, controlKeyExists := justifications.mapping[standardKey][controlKey]
 	if !controlKeyExists {
 		justifications.mapping[standardKey][controlKey] = Verifications{}
 	}
 	justifications.mapping[standardKey][controlKey] = append(
-		justifications.mapping[standardKey][controlKey], Verification{componentKey, verificationKey},
+		justifications.mapping[standardKey][controlKey], newVerification,
 	)
 	justifications.Unlock()
 }
 
 // LoadMappings loads a set of mappings from a component
 func (justifications *Justifications) LoadMappings(component *Component) {
-	var componentKey string
-	for _, satsifies := range *(component.Satisfies) {
-		for _, coveredBy := range satsifies.CoveredBy {
-			componentKey = coveredBy.ComponentKey
-			if componentKey == "" {
-				componentKey = component.Key
-			}
-			justifications.Add(satsifies.StandardKey, satsifies.ControlKey, componentKey, coveredBy.VerificationKey)
-		}
+	for _, satisfies := range *(component.Satisfies) {
+		justifications.Add(satisfies.StandardKey, satisfies.ControlKey, component.Key, satisfies)
 	}
 }
 
@@ -83,7 +76,7 @@ func (justifications *Justifications) Get(standardKey string, controlKey string)
 }
 
 //GetAndApply get a justification set and apply a generic function
-func (justifications *Justifications) GetAndApply(standardKey string, controlKey string, callback func(justifications Verifications)) {
+func (justifications *Justifications) GetAndApply(standardKey string, controlKey string, callback func(selectJustifications Verifications)) {
 	justifications.Lock()
 	callback(justifications.Get(standardKey, controlKey))
 	justifications.Unlock()
