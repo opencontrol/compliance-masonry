97866e20dd03c9c5f8e02404a31cec0597c5d789
James C. Scott <jamesc.scott@gsa.gov>
2016-03-22 15:00:54 -0400

    Add manager tests

---
+++ tools/vcs/manager_test.go
@@ -0,0 +1,66 @@
+package vcs
+
+import (
+	"testing"
+	"io/ioutil"
+	"os"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestClone(t *testing.T) {
+	var CloneTests = []struct {
+		url string
+		rev string
+		errorIsNull bool
+		errorString string
+	}{
+		{
+			// Sane check.
+			url: "https://github.com/opencontrol/compliance-masonry-go",
+			rev: "master",
+			errorIsNull: true,
+		},
+		{
+			// Sane check. No revision
+			url: "https://github.com/opencontrol/compliance-masonry-go",
+			errorIsNull: true,
+		},
+		{
+			// Can't init / detect the repo.
+			url: "https://myrepo/opencontrol/compliance-masonry-go",
+			rev: "master",
+			errorIsNull: false,
+			errorString: repoInitFailed,
+		},
+		{
+			// Can't init / detect the repo.
+			url: "http://user:name@github.com/opencontrol/compliance-masonry-go-blah",
+			rev: "master",
+			errorIsNull: false,
+			errorString: repoCloneFailed,
+		},
+		{
+			// Get a revision that doesn't exist.
+			url: "https://github.com/opencontrol/compliance-masonry-go",
+			rev: "master-ultimate-branch-that-never-exists",
+			errorIsNull: false,
+			errorString: repoCheckoutFailed,
+		},
+
+	}
+	for _, test := range CloneTests {
+		local, err := ioutil.TempDir("", "go-vcs")
+		if err != nil {
+			assert.Fail(t, err.Error())
+		}
+		err = Clone(test.url, test.rev, local)
+		if test.errorIsNull {
+			assert.Nil(t, err)
+		} else {
+			assert.NotNil(t, err)
+			assert.Contains(t, err.Error(), test.errorString)
+		}
+		os.RemoveAll(local)
+	}
+}
