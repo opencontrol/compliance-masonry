dedb8f88a6e4c5460c0b2a10e55d0e9dd1139db1
James C. Scott <jamesc.scott@gsa.gov>
2016-03-31 11:03:14 -0400

    Convert config package to ginkgo tests

--- config/parse_test.go
+++ config/parse_test.go
@@ -1,71 +1,92 @@
-package config
+package config_test
 
 import (
+	. "github.com/opencontrol/compliance-masonry-go/config"
+
+	. "github.com/onsi/ginkgo"
+	"github.com/opencontrol/compliance-masonry-go/config/common"
 	"github.com/opencontrol/compliance-masonry-go/config/common/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/vektra/errors"
-	"testing"
 )
 
-func TestBadInputsParse(t *testing.T) {
-	parser := new(mocks.SchemaParser)
-	// Nil data
-	_, err := Parse(parser, nil)
-	assert.Equal(t, ErrNoDataToParse, err)
+var _ = Describe("Parse", func() {
+	var (
+		parser *mocks.SchemaParser
+		err    error
+		schema common.BaseSchema
+	)
 
-	// Empty Data
-	data := []byte("")
-	_, err = Parse(parser, data)
-	assert.Equal(t, ErrNoDataToParse, err)
+	BeforeEach(func() {
+		parser = new(mocks.SchemaParser)
+	})
 
-	// Bad base parse
-	data = []byte("schema_version: @")
-	_, err = Parse(parser, data)
-	assert.Contains(t, err.Error(), ErrMalformedBaseYamlPrefix)
-
-	// Malformed yaml - wrong value type
-	data = []byte("schema_version: versionone")
-	_, err = Parse(parser, data)
-	assert.Equal(t, err, ErrCantParseSemver)
-
-	// Non-string version 0.0
-	data = []byte("schema_version: 1.0")
-	_, err = Parse(parser, data)
-	assert.Equal(t, ErrCantParseSemver, err)
-}
-
-func TestParseUnknownVersion(t *testing.T) {
-	parser := new(mocks.SchemaParser)
-	// Unknown version 0.0
-	data := []byte(`schema_version: "0.0.0"`)
-	_, err := Parse(parser, data)
-	assert.Equal(t, ErrUnknownSchemaVersion, err)
-}
-
-func TestParseV1_0_0(t *testing.T) {
-	// Test that ParseV1_0_0 is not called when only specifying 1.0 as version not the full 1.0.0
-	parser := new(mocks.SchemaParser)
-	data := []byte(`schema_version: "1.0"`)
-	mockSchema := new(mocks.BaseSchema)
-	parser.On("ParseV1_0_0", data).Return(mockSchema, nil)
-	_, _ = Parse(parser, data)
-	parser.AssertNotCalled(t, "ParseV1_0_0", data)
-
-	// Test that ParseV1_0_0 is called
-	parser = new(mocks.SchemaParser)
-	data = []byte(`schema_version: "1.0.0"`)
-	mockSchema = new(mocks.BaseSchema)
-	parser.On("ParseV1_0_0", data).Return(mockSchema, nil)
-	_, _ = Parse(parser, data)
-	parser.AssertCalled(t, "ParseV1_0_0", data)
-
-	// Test that ParseV1_0_0 is called but returns a failure
-	expectedError := errors.New("Can't parse yaml")
-	parser = new(mocks.SchemaParser)
-	data = []byte(`schema_version: "1.0.0"`)
-	mockSchema = new(mocks.BaseSchema)
-	parser.On("ParseV1_0_0", data).Return(mockSchema, expectedError)
-	_, err := Parse(parser, data)
-	parser.AssertCalled(t, "ParseV1_0_0", data)
-	assert.Equal(t, expectedError, err)
-}
+	Describe("bad input scenarios", func() {
+		It("should detect there's no data to parse when given nil data", func() {
+			schema, err = Parse(parser, nil)
+			assert.Equal(GinkgoT(), ErrNoDataToParse, err)
+		})
+		It("should detect there's no data to parse when given empty data", func() {
+			schema, err = Parse(parser, []byte(""))
+			assert.Equal(GinkgoT(), ErrNoDataToParse, err)
+		})
+		It("should detect when it's unable to unmarshal into the base type", func() {
+			schema, err = Parse(parser, []byte("schema_version: @"))
+			assert.Contains(GinkgoT(), err.Error(), ErrMalformedBaseYamlPrefix)
+		})
+		It("should detect when it's unable to determine the semver version because it is not in the format", func() {
+			schema, err = Parse(parser, []byte("schema_version: versionone"))
+			assert.Equal(GinkgoT(), err, ErrCantParseSemver)
+		})
+		It("should detect when it's unable to determine the semver version because the version is not in string quotes", func() {
+			schema, err = Parse(parser, []byte(`schema_version: 1.0`))
+			assert.Equal(GinkgoT(), err, ErrCantParseSemver)
+		})
+		It("should detect when the version is unknown", func() {
+			schema, err = Parse(parser, []byte(`schema_version: "0.0.0"`))
+			assert.Equal(GinkgoT(), err, ErrUnknownSchemaVersion)
+		})
+	})
+	Describe("ParseV1_0_0 scenarios", func() {
+		var (
+			data          []byte
+			expectedError error
+			mockSchema    *mocks.BaseSchema
+		)
+		BeforeEach(func() {
+			expectedError = nil
+			mockSchema = new(mocks.BaseSchema)
+		})
+		JustBeforeEach(func() {
+			parser.On("ParseV1_0_0", data).Return(mockSchema, expectedError)
+		})
+		Context("when the ParseV1_0_0 will not be called", func() {
+			It("should not call it when passing in 1.0", func() {
+				Parse(parser, []byte(`schema_version: "1.0"`))
+				parser.AssertNotCalled(GinkgoT(), "ParseV1_0_0", data)
+			})
+		})
+		Context("when the ParseV1_0_0 will is called", func() {
+			BeforeEach(func() {
+				data = []byte(`schema_version: "1.0.0"`)
+			})
+			Context("when ParseV1_0_0 is passed valid data", func() {
+				It("should call ParseV1_0_0", func() {
+					_, err = Parse(parser, data)
+					parser.AssertCalled(GinkgoT(), "ParseV1_0_0", data)
+					assert.Equal(GinkgoT(), expectedError, err)
+				})
+			})
+			Context("when ParseV1_0_0 is passed invalid data", func() {
+				BeforeEach(func() {
+					expectedError = errors.New("can't parse yaml")
+				})
+				It("should call ParseV1_0_0 but return an error", func() {
+					_, err = Parse(parser, data)
+					parser.AssertCalled(GinkgoT(), "ParseV1_0_0", data)
+					assert.Equal(GinkgoT(), expectedError, err)
+				})
+			})
+		})
+	})
+})
