c41e9b79c74800df087569d006cc507439dc77e1
James C. Scott <jamesc.scott@gsa.gov>
2016-04-04 13:47:35 -0400

    Add mapset ginkgo tests

--- tools/mapset/map_test.go
+++ tools/mapset/map_test.go
@@ -1,34 +1,25 @@
 package mapset
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
-func TestReserve(t *testing.T) {
-	m := Init()
+var _ = Describe("Map", func() {
+	var (
+		m = Init()
+	)
+	DescribeTable("Reserve", func(key, value string, expectedError error, expectedSuccess bool, expectedValue string) {
+		res := m.Reserve(key, value)
+		assert.Equal(GinkgoT(), expectedError, res.Error)
+		assert.Equal(GinkgoT(), expectedSuccess, res.Success)
+		assert.Equal(GinkgoT(), expectedValue, res.Value)
+	},
+		Entry("Regular reservation", "key1", "value", nil, true, "value"),
+		Entry("Repeat reservation", "key1", "value", ErrAlreadyExists, false, ""),
+		Entry("no key", "", "value", ErrEmptyInput, false, ""),
+		Entry("no value", "key1", "", ErrEmptyInput, false, ""),
+	)
 
-	// Regular reservation
-	res := m.Reserve("key1", "value")
-	assert.Equal(t, res.Error, nil)
-	assert.Equal(t, res.Success, true)
-	assert.Equal(t, res.Value, "value")
-
-	// Repeat
-	res = m.Reserve("key1", "value")
-	assert.Equal(t, res.Error, ErrAlreadyExists)
-	assert.Equal(t, res.Success, false)
-	assert.Equal(t, res.Value, "")
-
-	// bad inputs: no key
-	res = m.Reserve("", "value")
-	assert.Equal(t, res.Error, ErrEmptyInput)
-	assert.Equal(t, res.Success, false)
-	assert.Equal(t, res.Value, "")
-
-	// bad inputs: no Value
-	res = m.Reserve("key1", "")
-	assert.Equal(t, res.Error, ErrEmptyInput)
-	assert.Equal(t, res.Success, false)
-	assert.Equal(t, res.Value, "")
-}
+})
