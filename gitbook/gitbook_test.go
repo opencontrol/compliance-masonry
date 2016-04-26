78203fc91a67c0476aace5bec5995cd27497b960
Aidan Feldman <aidan.feldman@gsa.gov>
2016-04-25 20:00:46 -0400

    print filenames on test failure for easier debugging

--- gitbook/gitbook_test.go
+++ gitbook/gitbook_test.go
@@ -97,7 +97,7 @@ func TestBuildGitbook(t *testing.T) {
 			actualDataString := strings.Replace(string(actualData), "\\", "/", -1)
 			// Verify the expected text is the same as the actual text
 			if string(expectedData) != actualDataString {
-				t.Errorf("Expected: `%s`,\n Actual: `%s`", string(expectedData), string(actualData))
+				t.Errorf("Expected (%s):\n`%s`\nActual:\n`%s`", expectedfilePath, string(expectedData), string(actualData))
 			}
 		}
 	}
