6943f3d4bdd55138328166d537acdffd69212a66
James C. Scott <jamesc.scott@gsa.gov>
2016-05-12 18:11:24 -0400

    Adding behavior tests for diff

--- masonry-go_test.go
+++ masonry-go_test.go
@@ -135,6 +135,29 @@ var _ = Describe("Masonry CLI", func() {
 		})
 
 	})
+
+	Describe("Diff Commands", func() {
+		Describe("When the diff command is run", func() {
+			It("should let the user know that they have not described a certification and show how to use the command", func() {
+				output := Masonry("diff")
+				Eventually(output.Out.Contents).Should(ContainSubstring("Error: Missing Certification Argument"))
+			})
+		})
+		Describe("When the CLI is run with the `diff` command without opencontrols dir", func() {
+			It("should let the user know that there is no opencontrols/certifications directory", func() {
+				output := Masonry("diff", "LATO")
+				Eventually(output.Out.Contents).Should(ContainSubstring("Error: `" + filepath.Join("opencontrols", "certifications") + "` directory does exist"))
+			})
+		})
+		Describe("When the CLI is run with the `diff` command with a certification", func() {
+			It("should print the number of missing controls", func() {
+				output := Masonry(
+					"diff", "LATO",
+					"-o", filepath.Join("fixtures", "opencontrol_fixtures")).Wait(1 * time.Second)
+				Eventually(output.Out.Contents).Should(ContainSubstring("Number of missing controls:"))
+			})
+		})
+	})
 })
 
 func Masonry(args ...string) *Session {
