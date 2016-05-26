472725f206498e6cd4899d8a68f30513a7756a72
James C. Scott <jamesc.scott@gsa.gov>
2016-05-26 10:23:51 -0400

    go fmt errors_test

--- tools/version/errors_test.go
+++ tools/version/errors_test.go
@@ -6,8 +6,8 @@ import (
 
 	. "github.com/onsi/ginkgo"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/opencontrol/compliance-masonry/tools/constants"
+	"github.com/stretchr/testify/assert"
 )
 
 var _ = Describe("Errors", func() {
@@ -20,12 +20,11 @@ var _ = Describe("Errors", func() {
 	})
 
 	DescribeTable("String() for IncompatibleVersionError",
-	func(err IncompatibleVersionError, output string){
-		assert.Equal(GinkgoT(), output, err.Error())
-	},
+		func(err IncompatibleVersionError, output string) {
+			assert.Equal(GinkgoT(), output, err.Error())
+		},
 		Entry("Min version not needed", NewIncompatibleVersionError("file", "type", 2, constants.VersionNotNeeded, 1), "File: [file] uses version 2.00. Filetype: [type],  Max Version supported: 1.00"),
 		Entry("Max version not needed", NewIncompatibleVersionError("file", "type", 1, 2, constants.VersionNotNeeded), "File: [file] uses version 1.00. Filetype: [type],  Min Version supported: 2.00"),
 		Entry("Both max and min versions needed", NewIncompatibleVersionError("file", "type", 4, 2, 3), "File: [file] uses version 4.00. Filetype: [type],  Min Version supported: 2.00 Max Version supported: 3.00"),
-
 	)
 })
