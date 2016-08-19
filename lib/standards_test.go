0e1d033ca014ba7919e7b99f7ddfe3bce1977d70
James C. Scott <jamesc.scott@gsa.gov>
2016-08-19 10:52:36 -0400

    Increase test coverage for standards

---
+++ lib/standards_test.go
@@ -0,0 +1,33 @@
+package lib
+
+import (
+	"testing"
+	"github.com/opencontrol/compliance-masonry/lib/common/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/opencontrol/compliance-masonry/lib/common"
+)
+
+func TestGetStandard(t *testing.T) {
+	// Setup map
+	m := newStandards()
+	// Get nil component.
+	standard := m.Get("test")
+	assert.Nil(t, standard)
+	// Create mock component
+	newStandard := new(mocks.Standard)
+	newStandard.On("GetName").Return("test")
+	// Test add method
+	m.Add(newStandard)
+	// Try to retrieve the component again.
+	standard = m.Get("test")
+	assert.Equal(t, standard.GetName(), "test")
+}
+
+func TestBadLoadStandard(t *testing.T) {
+	// Setup map
+	m := newStandards()
+	ws := LocalWorkspace{Standards: m}
+	err := ws.LoadStandard("fake.file")
+	assert.NotNil(t, err)
+	assert.Equal(t, common.ErrStandardSchema, err)
+}
